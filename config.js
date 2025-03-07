da1260226276201fa4dfd6143e68bd7bd396aaf0
Ehteshum <ehteshum2002@gmail.com>
2025-03-07 21:12:43 +0600

    Updated frontend to use Railway URL and improved stream handling

--- config.js
+++ config.js
@@ -1,5 +1,16 @@
 require('dotenv').config();
 
 const CONFIG = {
-    streamUrl: process.env.STREAM_URL || "http://kst.moonplex.net:8080/CH2/tracks-v1a1/mono.m3u8"
+    port: process.env.PORT || 3000,
+    streamUrl: process.env.STREAM_URL || 'http://kst.moonplex.net:8080',
+    nodeEnv: process.env.NODE_ENV || 'development',
+    streamPath: '/CH2/tracks-v1a1/mono.m3u8',
+    cors: {
+        origin: '*',
+        methods: ['GET', 'HEAD', 'OPTIONS'],
+        allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'Range'],
+        exposedHeaders: ['Content-Length', 'Content-Range', 'Accept-Ranges']
+    }
 };
+
+module.exports = CONFIG;
